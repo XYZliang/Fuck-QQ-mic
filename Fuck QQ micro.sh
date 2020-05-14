7f488ce3325c6b48d4195a818a621c388bb4dc23
张亮 <59312540+XYZliang@users.noreply.github.com>
2020-05-14 17:29:51 +0800

    V1.2

--- Fuck QQ micro.sh
+++ Fuck QQ micro.sh
@@ -3,13 +3,13 @@
 
 while true
 do
-        process=`ps aux | grep QQ | grep -v grep`;
+        process=`ps aux | grep /Applications/QQ.app/Contents/MacOS/QQ | grep -v grep`;
 
         if [ "$process" == "" ]; then
-                echo "未检测到QQ，请确保QQ已经打开，将在5秒后重新检测。";
+                echo "未检测到QQ主程序，请确保QQ已经打开，将在5秒后重新检测。";
                 sleep 5;
         else
-                echo "检测到QQ，正在执行";
+                echo "检测到QQ主程序，正在执行脚本，可能需要输入密码。";
 		printf "p *(char*)(void(*)())AudioDeviceDuck=0xc3\nq" | lldb -n QQ
                 break;
         fi
